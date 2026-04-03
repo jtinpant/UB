b3fc8b771620e7c784ed99bb6553a368b269f3f5
kranpant <110833432+kranpant@users.noreply.github.com>
2026-04-03 11:25:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,22 +5,15 @@ if os.path.exists(".env"):
     load_dotenv(".env")
 
 # ------------------------------ REQUIRED ------------------------------ #
-
 API_ID = int(os.getenv("API_ID", "27433131"))
 API_HASH = os.getenv("API_HASH", "7f8d967471ccadf83df1f199769b43e7")
-
-# Your New Bot Token
 BOT_TOKEN = os.getenv("BOT_TOKEN", "8579256317:AAGH40uDAKZXyW8rxDa-n0Sp5NGkUPaw5-4")
-
-# Your New Owner ID
 OWNER_ID = int(os.getenv("OWNER_ID", "5533647702"))
 
 # ------------------------------ SETTINGS ------------------------------ #
-
 hl = os.getenv("CMD_HNDLR", ".")
-
-SUDO_USERS = [OWNER_ID] # Owner is automatically sudo
+SUDO_USERS = [OWNER_ID] 
 
 # ------------------------------ ASSETS ------------------------------ #
-
 EXTRA_IMG = os.getenv("EXTRA_IMG", "https://files.catbox.moe/uufiry.jpg")
+UPSTREAM_REPO = "https://github.com/lavish-pro/SMOKER-USERBOT"
